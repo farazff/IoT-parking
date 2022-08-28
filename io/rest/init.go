ad10019ee6e2f9bf9e300b9687878c132deb429a
Faraz Farangizadeh <faraz.farangizadeh@snapp.cab>
2022-08-28 17:46:55 +0430

    add routes

--- io/rest/init.go
+++ io/rest/init.go
@@ -5,5 +5,27 @@ import (
 )
 
 func init() {
-	rest.EchoGet("/info", hello)
+	rest.EchoPost("/v1/parking", createParking, apiKey)
+	rest.EchoGet("/v1/parking/:id", getParking, apiKey)
+	rest.EchoGet("/v1/parkings", getParkings, apiKey)
+	rest.EchoPut("/v1/parking/:id", updateParking, apiKey)
+	rest.EchoDelete("/v1/parking/:id", deleteParking, apiKey)
+
+	rest.EchoPost("/v1/zone", createZone, apiKey)
+	rest.EchoGet("/v1/zone/:id", getZone, apiKey)
+	rest.EchoGet("/v1/zones", getZones, apiKey)
+	rest.EchoPut("/v1/zone/:id", updateZone, apiKey)
+	rest.EchoDelete("/v1/zone/:id", deleteZone, apiKey)
+
+	rest.EchoPost("/v1/parkingAdmin", createParkingAdmin, apiKey)
+	rest.EchoGet("/v1/parkingAdmin/:id", getParkingAdmin, apiKey)
+	rest.EchoGet("/v1/parkingAdmins", getParkingAdmin, apiKey)
+	rest.EchoPut("/v1/parkingAdmin/:id", updateParkingAdmin, apiKey)
+	rest.EchoDelete("/v1/parkingAdmin/:id", deleteParkingAdmin, apiKey)
+
+	rest.EchoPost("/v1/systemAdmin", createSystemAdmin, apiKey)
+	rest.EchoGet("/v1/systemAdmin/:id", getSystemAdmin, apiKey)
+	rest.EchoGet("/v1/systemAdmins", getsyStemAdmin, apiKey)
+	rest.EchoPut("/v1/systemAdmin/:id", updateSystemAdmin, apiKey)
+	rest.EchoDelete("/v1/systemAdmin/:id", deleteSystemAdmin, apiKey)
 }
--- io/rest/routes.go
+++ io/rest/routes.go
@@ -1,11 +1,9 @@
 package rest
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "hello")
+
 }
