56f297ddb4cec8c835dd062c0dfaa36e72872c6a
Faraz Farangizadeh <faraz.farangizadeh@snapp.cab>
2023-07-18 15:55:26 +0330

    implement user update route

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/okian/servo/v2"
 	_ "github.com/okian/servo/v2/config"
 	_ "github.com/okian/servo/v2/db"
-	_ "github.com/okian/servo/v2/kv/redis"
 	_ "github.com/okian/servo/v2/lg"
 	_ "github.com/okian/servo/v2/rest"
 	"github.com/spf13/cobra"
